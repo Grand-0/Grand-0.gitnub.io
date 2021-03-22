3078701c3f96772d208e11926b3b70cf47bd9a53
Grand-0 <fomovichf85@mail.ru>
2021-03-22 13:19:54 +0300

    Updates

---
+++ precache-manifest.eb179c47c836df14873ee5546f3e2b33.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0ada3f3a080fc7740ed9712fe1d756",
+    "url": "/Grand-0.gitnub.io/index.html"
+  },
+  {
+    "revision": "68ac9222abccfd4ad29f",
+    "url": "/Grand-0.gitnub.io/static/css/2.ed890e5e.chunk.css"
+  },
+  {
+    "revision": "a96b52d35f09ac03fa2b",
+    "url": "/Grand-0.gitnub.io/static/css/main.2f82d4f0.chunk.css"
+  },
+  {
+    "revision": "68ac9222abccfd4ad29f",
+    "url": "/Grand-0.gitnub.io/static/js/2.8fb224cb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Grand-0.gitnub.io/static/js/2.8fb224cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a96b52d35f09ac03fa2b",
+    "url": "/Grand-0.gitnub.io/static/js/main.66f73344.chunk.js"
+  },
+  {
+    "revision": "2206061a584512e3cd99",
+    "url": "/Grand-0.gitnub.io/static/js/runtime-main.f34fff56.js"
+  },
+  {
+    "revision": "3181a4ef3407c85343bd3826806ede98",
+    "url": "/Grand-0.gitnub.io/static/media/ade5280032e91455765d7d400df4741e-lotus-flower-icon-lotus-icons-by-vexels.3181a4ef.png"
+  },
+  {
+    "revision": "04919054de26ff9f6ebb1ac7269466f4",
+    "url": "/Grand-0.gitnub.io/static/media/mountain-icon-png.04919054.png"
+  }
+]);
